ac2d89d8adf744d329129fa816817f89f37236a8
Zakharov Denis <zakharov.denis22@gmail.com>
2022-03-28 21:00:20 +0300

    flake

--- app/routes/v1/handlers/lists.py
+++ app/routes/v1/handlers/lists.py
@@ -1,9 +1,4 @@
-from datetime import datetime
-from uuid import UUID
+from fastapi import APIRouter
 
-from fastapi import APIRouter, status
-
-from app.routes.v1.models.lists import GetList, AddList
-from services.v1.lists import ListsService
 
 router = APIRouter()
